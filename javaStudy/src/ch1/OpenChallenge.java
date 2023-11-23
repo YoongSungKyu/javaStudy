29c3076638868b6042a10bcbd5ef08446ef381f6
YoongSungKyu <151002261+YoongSungKyu@users.noreply.github.com>
2023-11-23 11:28:23 +0900

    x

---
+++ javaStudy/src/ch1/OpenChallenge.java
@@ -0,0 +1,13 @@
+package ch1;
+
+// 자신의 이름 나이 학과 정보를 출력하는 응용프로그램 작성
+public class OpenChallenge {
+    public static void main(String[] args){
+        String old = "years old";
+        System.out.println("윤성규");
+        System.out.println("24 " + old);
+        System.out.println("Department of Computer Engineering");
+
+
+    }
+}
---
+++ javaStudy/src/ch1/Pratice_01.java
@@ -0,0 +1,11 @@
+package ch1;
+
+//출력물 : Welcome !! 자바 세계에 오신 것을 환영합니다.
+
+public class Pratice_01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome!!");
+        System.out.println("자바 세계에 오신 것을 환영합니다");
+
+    }
+}
